6f52fed8ca6bf58f36d65bc8441a1f037b846939
daliadalia22 <98914719+daliadalia22@users.noreply.github.com>
2022-02-05 01:11:17 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World");
+	byte num = 2;
+    int i = 238888;
+    long l = 787868;
+    float num_2 = 324.234f;
+    double num_3 = 787.2f;
+
+    char sym = 'A';
+    boolean is_happy = false;
+    String str = "Hello World";
+
+    System.out.println ("variable = "+ is_happy);
+
     }
 }
