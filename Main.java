427b4957675ff936af576598b7570d4e91193c4e
daliadalia22 <98914719+daliadalia22@users.noreply.github.com>
2022-02-05 02:44:30 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -33,5 +33,29 @@ public class Main {
         dev = num_9 / num_5;
         System.out.println (dev);
 
+        int x = 1;
+        System.out.println(x++); // => 6
+        System.out.println(x);
+        int y = 2;
+        System.out.println(y--); // => 6
+        System.out.println(y);
+
+        long l1 = 1, l2 = 8, suml, minl, multl, devl;
+        suml = l1 + l2;
+        System.out.println (suml);
+
+        minl = l1 - l2;
+        System.out.println (minl);
+        multl = l1 * l2;
+        System.out.println (multl);
+        devl = l1 / l2;
+        System.out.println (devl);
+
+        long xl = 1;
+        System.out.println(xl++); // => 6
+        System.out.println(xl);
+        int yl = 2;
+        System.out.println(yl--); // => 6
+        System.out.println(yl);
     }
 }
