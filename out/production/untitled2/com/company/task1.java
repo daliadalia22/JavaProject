c7d480dc84100d3c05fc0422a76669ebe504d51b
daliadalia22 <98914719+daliadalia22@users.noreply.github.com>
2022-02-12 01:20:57 +0300

    Add files via upload

---
+++ out/production/untitled2/com/company/task1.java
@@ -0,0 +1,22 @@
+package week2;
+
+public class task1 {
+    public static void main(String[]  args) {
+        int n = 11;
+                if(1 < n < 25)
+        {System.out.println("Плохо");
+        }
+        else if (26 < n < 50)
+        {System.out.println ("Ниже среднего");
+        }
+        else if (51 < n < 75) {
+            System.out.println("Выше среднего");
+        }
+        else  if (n <= 100) {
+            System.out.println("Отлично");
+        }
+          else if (n!= 100) {
+            System.out.println("Ошибка");}
+        }
+          }
+
