1c675e46f39bf0b9c9fc3e890b244cf98d52cf22
daliadalia22 <98914719+daliadalia22@users.noreply.github.com>
2022-02-12 10:48:16 +0300

    Add files via upload

--- out/production/untitled2/com/company/task2.java
+++ out/production/untitled2/com/company/task2.java
@@ -1,24 +1,20 @@
 package week2;
 
-import static java.lang.StringUTF16.length;
 
 public class task2 {
     public static void main(String[]  args) {
         String[]  names = {"John","Bob", "Mary","Peter", "Sara"};
-        int length;
-        length = names.length;
-        for (int n = 0; n < length ; n++)
-        if(length(names[n])) < 4); {
-            System.out.println("Ваше имя короче четырех букв");
+        int length = names.length;
+        for (int i = 0; i < names.length ; i++)
+        if(length(names(i)) < 4)
+        {System.out.println("Ваше имя короче четырех букв");
         }
-        else if(length(names[n])) > 4);
-{
-                System.out.println("Ваше имя длиннее четырех букв");
-            }
-        else
-        {
-            System.out.println("Ваше имя состоит из 4 букв");
+        else if(length(names[i]) > 4)
+        {System.out.println("Ваше имя длиннее четырех букв");
         }
+        else if(length(names[i]) == 4)
+        { System.out.println("Ваше имя состоит из 4 букв");
         }
-    }
+
+    }}
 
