599044cdebce7afc811af2d3b60d613dbb9b1196
daliadalia22 <daliajaafar2021@gmail.com>
2022-02-23 23:37:32 +0300

    34

---
+++ src/com/company/Task1.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int mark = sc.nextInt();
+        if ((mark >=1) && (mark <=25))  {
+                System.out.println("Плохо");
+            }
+        else if ((mark >= 26) && (mark <= 50)) {
+                System.out.println("Ниже среднего");
+            }
+        else if ((mark >= 51) && (mark <= 75)) {
+                System.out.println("Выше среднего");
+            }
+        else if ((mark >= 75) && (mark <= 100)) {
+                System.out.println("Отлично");
+
+        }
+    }
+
+}
---
+++ src/com/company/Task2.java
@@ -0,0 +1,28 @@
+package com.company;
+public class Task2 {
+    public static void main(String[] args) {
+        String[] names = {"John", "Bob", "Mary", "Peter", "Sara"};
+        int length = names.length;
+        System.out.println("For");
+        for (int i = 0; i < length; i++) {
+            if (names[i].length() > 10) {
+                break;
+            }
+            if (names[i].length() < 2) {
+                continue;
+            }
+            if (names[i].length() < 4) {
+                System.out.println(names[i] + ",ваше имя короче 4 букв");
+            } else if (names[i].length() > 4) {
+                System.out.println(names[i] + ",ваше имя длиннее 4 букв");
+            } else {
+                System.out.println(names[i] + ",ваше имя состоит из 4 букв");
+
+            }
+        }
+    }
+}
+
+
+
+
