15f737872bb3f957d5bf01b3b80c45c18f4bd01e
daliadalia22 <daliajaafar2021@gmail.com>
2022-02-26 03:08:18 +0300

    34

---
+++ src/com/company/week4/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Week4NewYorkCoffeShop shop1 = new Week4NewYorkCoffeShop[];
+        int NewYork;
+        shop1.city = NewYork;
+        System.out.println("NewYork");
+
+    }
+}
