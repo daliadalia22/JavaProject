da0717f07ae90174e80f44190a4f17fc37467252
AlehShyliuk <47559090+AlehShyliuk@users.noreply.github.com>
2022-02-27 12:09:02 +0400

    Feedback

--- src/com/company/week4/Main.java
+++ src/com/company/week4/Main.java
@@ -1,5 +1,7 @@
 package com.company.week4;
 import java.util.Scanner;
+// в целом сделан только 1 пункт задачи.
+//так как мы делаем полноценное приложение, то этот класс лучше назвать как-то осознанно, типа CoffeeShop
 public class Main {
     public static void main(String[] args) {
         System.out.println("Из какого Вы города? Baghdad - 1, NewYork - 2, Moscow -3" );
@@ -7,9 +9,14 @@ public class Main {
         int city = sc.nextInt();
         if (city == 1) {
             System.out.println("Baghdad");
+            //непонятно зачем помещать код ниже в отдельные фигурные скобки {}, это лишнее
             {Week4BaghdadCoffeShop shop1 = new Week4BaghdadCoffeShop();
+                 
+                 //переменные должны называться с маленькой буквы
                  int Baghdad = 0;
                 shop1.city3 = Baghdad;
+                //здесь тоже непонятно зачем помещать код ниже в отдельные фигурные скобки {}, это лишнее
+                //это относится и к остальныму коду, тот что ниже
                 {System.out.println("Your coffee is ready - Baghdad team");
                 }
 
