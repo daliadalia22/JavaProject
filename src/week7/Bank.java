f14a1c5f582e89d63af8c89639e025220fcf98ef
daliadalia22 <daliajaafar2021@gmail.com>
2022-04-03 07:48:01 +0300

    34

--- src/week7/Bank.java
+++ src/week7/Bank.java
@@ -2,7 +2,6 @@ package week7;
 
 public class Bank {
     private User[] users;
-
     public void serializeUsers() {
 
     }
@@ -11,10 +10,7 @@ public class Bank {
         return new User[0];
     }
     public void start() {
-        
-        //не уверен что ты пыталась тут сделать, но тут должен вызываться метод showStartMenu класса BankMenu
-        //для этого в этом классе не хватает поля BankMenu
-        final var users = deserializeUsers();
+    final var users = deserializeUsers();
       }
       public Boolean doLogin(String email, String password) {
         for (User user: users) {
@@ -29,5 +25,4 @@ public class Bank {
 public void doRegister(User user) {
     }
     
-    //также тут должны быть методы doLogin и doRegister
 }
