5b655c1f727e1d4beea61808ea5b4981d3ff5aaf
AlehShyliuk <47559090+AlehShyliuk@users.noreply.github.com>
2022-04-03 13:36:47 +0400

    Feedback

--- src/week7/Bank.java
+++ src/week7/Bank.java
@@ -1,28 +1,32 @@
 package week7;
 
 public class Bank {
+    //мы уже знаем про коллекции, поэтому массивы не используем, в остальном методы не реализованы
     public User[] users;
+    
     public void serializeUsers() {
 
     }
+    
     public User[] deserializeUsers() {
-
         return new User[0];
     }
+    
     public void start() {
-    final var users = deserializeUsers();
-      }
-      public Boolean doLogin(String email, String password) {
+        final var users = deserializeUsers();
+    }
+    
+    public Boolean doLogin(String email, String password) {
         for (User user: users) {
             if (user.getEmail() == email && user.getPassword() == password){
-                 {
-                    return true;
-                }
+                return true;
             }
         }
-        return true;
-      }
-public void doRegister() {
+        return false;
+    }
+    
+    public void doRegister() {
+        
     }
 
     public void doLogin() {
