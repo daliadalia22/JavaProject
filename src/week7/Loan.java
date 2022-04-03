7ae4f7ac841b8a13e09df7e79c5ab555bc1b5e7f
daliadalia22 <daliajaafar2021@gmail.com>
2022-04-03 08:32:24 +0300

    34

--- src/week7/Loan.java
+++ src/week7/Loan.java
@@ -2,9 +2,41 @@ package week7;
 
 import java.util.Date;
 public class Loan {
-    Date issueDate;
+    String issueDate;
     Double sum;
     Double percent;
     Integer duration;
     Double monthlyPayment;
+    public String toString() {
+        return ("Issue Date: " + issueDate + "/m Sum:" + sum + "/m Percent" + percent + "/m duration" + duration +
+                "/m monthlyPayment" + monthlyPayment);
     }
+    public String getIssueDate () {return issueDate;
+    }
+    public void setIssueDate (String issueDate) {
+        this.issueDate = issueDate;
+    }
+    public double getSum () {return sum;
+    }
+    public void setSum (Double sum) {
+        this.sum = sum;
+    }
+    public Double getPercent () {
+        return percent;
+    }
+    public void setPercent (Double percent) {
+        this.percent = percent;
+        }
+    public Integer getDuration () {
+        return duration;
+    }
+    public void setDuration (Integer duration) {
+        this.duration = duration;
+    }
+    public Double getMonthlyPayment () {
+        return monthlyPayment;
+    }
+    public void setMonthlyPayment (Double monthlyPayment) {
+        this.monthlyPayment = monthlyPayment;
+    }
+}
